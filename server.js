32e2cdd92a0a2b6a9374d1f47758fbb9fdf8ee21
Chris Kratz <Ckratz17@gmail.com>
2022-07-26 19:42:20 -0400

    added query to the functions to adjust the table accordingly

--- server.js
+++ server.js
@@ -1,29 +1,14 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
-
 //import msql2, inquirer
-const mysql = require("mysql2")
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
 
 //connect to database
 const db = mysql.createConnection(
     {
       host: 'localhost',
+
+      port: 3306,
       // MySQL username,
       user: 'root',
       // MySQL password
@@ -44,11 +29,33 @@ function menu() {
             "View all employees",
             "View all roles",
             "Add employee",
+            "Add department",
             "Add Role",
             "Update an employee's role",
         ]
     }).then(function (answer) {
+        switch (answer.menu) {
+            case "View all departments": showDepts();
+            break;
 
+            case "View all employees": showEmployees();
+            break;
+
+            case "View all roles": showRoles();
+            break;
+
+            case "Add employee": addEmployees();
+            break;
+
+            case "Add department": addDepts();
+            break;
+
+            case "Add Role": addRole();
+            break;
+
+            case "Update an employee's role": updateRole();
+            break;
+        }
     })
 }
 //function to show all departments, roles and employees
@@ -60,7 +67,12 @@ function showDepts() {
 }
 
 function showEmployees() {
-    db.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name AS department,`)
+    db.query
+        (`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name AS department,
+          role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department on role.department_id = department.id`, (err, res) => {
+            console.table(res)
+            menu()
+        })
 }
 
 function showRoles() {
@@ -79,7 +91,10 @@ function addDepts() {
        message: "What is the name of your new department?"
     }).then (function(res) {
         const newDept = res.newDepts
-        db.query(``)
+        db.query(`INSERT INTO department (department_name) VALUES ("${newDept}")`, (err, res) => {
+            console.table(res)
+            menu()
+        })
     })
 
 }
@@ -111,7 +126,10 @@ function addEmployees() {
         const lastName = res.lastName
         const roleId = res.roleId
         const managerId = res.managerId
-        db.query(``)
+        db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${firstName}", "${lastName}", "${roleId}", "${managerId}")`, (err, res) => {
+            console.table(res)
+            menu()
+        })
         
     })
 }
@@ -137,6 +155,31 @@ function addRole() {
         const title = res.empTitle
         const salary = res.empSalary
         const deptId = res.empId
-        db.query()
+        db.query(`INSERT INTO role (title, salary, department_id) VALUES ("${title}", "${salary}", "${deptId}")`, (err, res) => {
+            console.table(res)
+            menu()
+        })
+    })
+}
+
+function updateRole() {
+    inquirer.prompt([
+        {
+         type: "input",
+         name: "emplUpdate",
+         message: "What is the employees new id?"
+        },
+        {
+         type: "input",
+         name: "newRole",
+         message: "What is the id for the new role?"
+        }
+    ]).then(function (res) {
+        const updateEmpl = res.emplUpdate
+        const newRole = res.newRole
+        db.query(`UPDATE employee SET role_id = "${newRole}" Where id = "${updateEmpl}"`, (err, res) => {
+            console.table(res)
+            menu()
+        })
     })
 }
